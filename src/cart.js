e8de9dadeb777975312ccdde13106c2d41cb4c54
Eric Tendian <erictendian@gmail.com>
2016-10-31 01:16:46 -0500

    Updating for Spring 2017

--- src/cart.js
+++ src/cart.js
@@ -205,7 +205,7 @@ var cart = {
     $('#register-section a').addClass('loading');
     for (var i in this._semesterCRNs[this._currentSemester]) {
       for (var j = 0; j < this._semesterCRNs[this._currentSemester][i].length; j++) {
-        $.get('https://cors-anywhere.herokuapp.com/https://ssb.iit.edu/banr/bwckschd.p_disp_detail_sched?term_in=201620&crn_in='+this._semesterCRNs[this._currentSemester][i][j], function (data, status, jqxhr) {
+        $.get('https://cors-anywhere.herokuapp.com/https://ssb.iit.edu/banr/bwckschd.p_disp_detail_sched?term_in=201720&crn_in='+this._semesterCRNs[this._currentSemester][i][j], function (data, status, jqxhr) {
           var crn = jqxhr.responseURL.match(/(.*?)crn_in=(.*)/)[2];
           var seats = data.match(/<TR>\n<TH CLASS="ddlabel" scope="row" ><SPAN class="fieldlabeltext">Seats<\/SPAN><\/TH>\n<TD CLASS="dddefault">([0-9]+)<\/TD>\n<TD CLASS="dddefault">([0-9]+)<\/TD>\n<TD CLASS="dddefault">([0-9]+)<\/TD>\n<\/TR>/);
           var waitlist = data.match(/<TR>\n<TH CLASS="ddlabel" scope="row" ><SPAN class="fieldlabeltext">Waitlist Seats<\/SPAN><\/TH>\n<TD CLASS="dddefault">([0-9]+)<\/TD>\n<TD CLASS="dddefault">([0-9]+)<\/TD>\n<TD CLASS="dddefault">([0-9]+)<\/TD>\n<\/TR>/);
